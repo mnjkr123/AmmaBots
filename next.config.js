83524c0cefc18ddcf58c7c2afc1a011fb421bd2a
mnjkr123 <ekmtutor@gmail.com>
2024-09-28 17:37:03 -0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ module.exports = {
             https: require.resolve('https-browserify'),
             querystring: require.resolve('querystring-es3'),
             stream: require.resolve('stream-browserify'),
+            net: false, // Disable 'net' module
+            tls: false, // Disable 'tls' module
+            child_process: false, // Disable 'child_process' module
         };
 
         // Add the NodePolyfillPlugin to the webpack plugins
