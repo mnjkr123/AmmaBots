ba0c856e62ec0796ef01516c97df9181e1a3912b
mnjkr123 <ekmtutor@gmail.com>
2024-09-28 17:32:14 -0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 module.exports = {
     webpack: (config) => {
         config.resolve.fallback = {
@@ -8,6 +10,10 @@ module.exports = {
             querystring: require.resolve('querystring-es3'),
             stream: require.resolve('stream-browserify'),
         };
+
+        // Add the NodePolyfillPlugin to the webpack plugins
+        config.plugins.push(new NodePolyfillPlugin());
+
         return config;
     },
 };
